fe8ff1f9cfd211e16b34e5bd030338c02093ec22
MACKENA05 <mackenamuthui@gmail.com>
2025-08-05 16:20:02 +0300

    first commit

---
+++ src/main/java/com/monicah/Umoja_Welfare/UmojaWelfareApplication.java
@@ -0,0 +1,13 @@
+package com.monicah.Umoja_Welfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmojaWelfareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmojaWelfareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monicah/Umoja_Welfare/UmojaWelfareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monicah.Umoja_Welfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmojaWelfareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
