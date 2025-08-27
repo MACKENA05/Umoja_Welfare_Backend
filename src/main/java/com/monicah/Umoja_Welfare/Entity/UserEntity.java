ecc3e7e67598ada8286533aadc984e02626ddb5a
reuben <Kibara>
2025-08-27 20:46:19 +0300

    Current changes

--- src/main/java/com/monicah/Umoja_Welfare/Entity/UserEntity.java
+++ src/main/java/com/monicah/Umoja_Welfare/Entity/UserEntity.java
@@ -1,4 +1,9 @@
 package com.monicah.Umoja_Welfare.Entity;
 
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Repository;
+
+@Entity
+@Repository
 public class UserEntity {
 }
